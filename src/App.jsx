72673fefb0299161b1d6e9ef7318833615fece70
Yusupov Elbek <elbeky829@gmail.com>
2023-09-30 00:43:26 +0500

    adding function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./Style.scss";
+import Form from "react-bootstrap/Form";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import { RiContactsFill } from "react-icons/ri";
+import { BsFillTelephoneFill } from "react-icons/bs";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [firstName, setFirstName] = useState("");
+  const [firstNameArr, setFirstNameArr] = useState([]);
+
+  const [lastName, setLastName] = useState("");
+  const [lastNameArr, setLastNameArr] = useState([]);
+
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [phoneNumberArr, setPhoneNumberArr] = useState([]);
+
+  function getValue(e) {
+    setFirstNameArr([...firstNameArr, firstName]);
+    setLastNameArr([...lastNameArr, lastName]);
+    setPhoneNumberArr([...phoneNumberArr, phoneNumber]);
+    e.preventDefault();
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <form>
+        <Form.Control
+          type="text"
+          style={{ width: "250px" }}
+          placeholder="First name"
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <Form.Control
+          type="text"
+          style={{ width: "250px" }}
+          placeholder="Last name"
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <Form.Control
+          type="tel"
+          style={{ width: "250px" }}
+          placeholder="Phone number"
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <Button variant="outline-primary" onClick={getValue} type="submit">
+          Add contact
+        </Button>
+      </form>
+      <div className="contact__list">
+        <div className="contact__list__title">
+          <h3>
+            <RiContactsFill />
+            First name
+          </h3>
+          <h3>
+            <RiContactsFill />
+            Last name
+          </h3>
+          <h3>
+            <BsFillTelephoneFill />
+            Phone number
+          </h3>
+        </div>
+        <div className="contact__value">
+          {
+          firstNameArr.map((e, index) => (
+            <p>{index}{e}</p>
+          ))}
+          
+          {
+          lastNameArr.map((e) => (
+            <span>{e}</span>
+          ))}
+
+          {
+          phoneNumberArr.map((e) => (
+            <span>{e}</span>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
